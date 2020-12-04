1b23942e61ed0b9be6c956c97a304d67d683975d
TB5zhh <654154090@qq.com>
2020-12-04 15:32:45 +0800

    update local.c

--- src/local.c
+++ src/local.c
@@ -15,13 +15,16 @@ int main()
 {
     struct timeval a;
     struct Data *data = malloc(sizeof(struct Data));
+    memset(data, 1, sizeof(struct Data));
     struct Data *data1 = malloc(sizeof(struct Data));
+    memset(data1, 1, sizeof(struct Data));
     int i;
     for (i = 0; i < TEST_TIME; i++)
     {
         memset(data->data, 0, sizeof(data->data));
         snprintf(data->data, sizeof(data->data), "%s, num:%d", "hello world!", i);
         gettimeofday(&data->tv, NULL);
+        snprintf(data->data, sizeof(data->data), "%s, num:%d", "hello world!", i);
         memcpy(data1, data, sizeof(struct Data));
         gettimeofday(&a, NULL);
         printf("time:%fus, receive:%s\n", difftimeval(&a, &data1->tv), data1->data);
--- src/mpi_example.c
+++ src/mpi_example.c
@@ -28,27 +28,68 @@ int main(int argc, char **argv)
 
 	if (my_rank == 0)
 	{
+		struct Data *datas = malloc(TEST_TIME * sizeof(struct Data));
+		memset(datas, 1, TEST_TIME * sizeof(struct Data));
+		for (int i = 0; i < TEST_TIME; i++)
+		{
+			struct Data *data = datas + i;
+			// printf("Address of data of rank0: %p, index: %d\n", data, i);
+			struct timeval tv;
+			MPI_Recv(data, sizeof(struct Data), MPI_BYTE, 1, 1, MPI_COMM_WORLD, &stat);
+			snprintf(data->data, sizeof(data->data), "%s, num:%d", "hello world!", i);
+			gettimeofday(&tv, NULL);
+			printf("time:%fus, receive:%s\n", difftimeval(&tv, &data->tv), data->data);
+			
+			printf("Pause at 0\n");
+			getchar();
+		}
+		free(datas);
+
+		printf("=======================\n");
+
 		for (int i = 0; i < TEST_TIME; i++)
 		{
 			struct Data *data = malloc(sizeof(struct Data));
+			memset(data, 1, sizeof(struct Data));
+			// printf("Address of data of rank0: %p, index: %d\n", data, i);
 			struct timeval tv;
 			MPI_Recv(data, sizeof(struct Data), MPI_BYTE, 1, 1, MPI_COMM_WORLD, &stat);
+			snprintf(data->data, sizeof(data->data), "%s, num:%d", "hello world!", i);
 			gettimeofday(&tv, NULL);
 			printf("time:%fus, receive:%s\n", difftimeval(&tv, &data->tv), data->data);
+
+			// printf("Pause at 0\n");
+			// getchar();
+
 			free(data);
 		}
 	}
 	else if (my_rank == 1)
 	{
-		struct Data *data = malloc(sizeof(struct Data));
+		struct Data *datas = malloc(TEST_TIME * sizeof(struct Data));
+		memset(datas, 1, TEST_TIME * sizeof(struct Data));
 		for (int i = 0; i < TEST_TIME; i++)
 		{
+			struct Data *data = datas + i;
+			// printf("Address of data of rank1: %p, index: %d\n", data, i);
 			memset(data->data, 0, sizeof(data->data));
 			snprintf(data->data, sizeof(data->data), "%s, num:%d", "hello world!", i);
 			gettimeofday(&data->tv, NULL);
 			MPI_Send(data, sizeof(struct Data), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
 		}
-		free(data);
+		free(datas);
+
+		for (int i = 0; i < TEST_TIME; i++)
+		{
+			struct Data *data = malloc(sizeof(struct Data));
+			memset(data, 1, sizeof(struct Data));
+			// printf("Address of data of rank1: %p, index: %d\n", data, i);
+			memset(data->data, 0, sizeof(data->data));
+			snprintf(data->data, sizeof(data->data), "%s, num:%d", "hello world!", i);
+			gettimeofday(&data->tv, NULL);
+			MPI_Send(data, sizeof(struct Data), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
+			free(data);
+		}
 	}
 
 	if (my_rank == 0)
